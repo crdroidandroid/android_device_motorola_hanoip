b23470864214b23375839d1edbc6717f20482ae2
KevinFrax <kevin.vm1511@gmail.com>
2024-03-31 22:22:04 +0000

    hanoip: I added vendorsetup.sh to clone missing packages

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 hardware/motorola
+
+git clone https://github.com/LineageOS/android_system_qcom system/qcom
+
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
