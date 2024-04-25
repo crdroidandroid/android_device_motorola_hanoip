7f64f6aa70f0afb15d526df87e17ebd4ae511efa
KevinFrax <kevin.vm1511@gmail.com>
2024-04-25 07:07:08 +0000

    hanoip: Use vendorsetup.sh to clone missing packages and lunch

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clonar el repositorio packages_apps_MotoActions desde la rama fourteen-qpr2 con profundidad 1
+git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 packages/apps/MotoActions
+
+# Clonar el repositorio android_system_qcom en el directorio system/qcom
+git clone https://github.com/LineageOS/android_system_qcom system/qcom
+
+# Clonar el repositorio packages_apps_ViPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# Configurar el uso de ccache
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+ccache -o compression=true
+
+# Desayunar el dispositivo hanoip
+breakfast hanoip
