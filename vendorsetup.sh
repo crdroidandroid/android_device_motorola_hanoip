b9de19c34912650f8ce28e6296c920081ca58ed2
Kevin Alfonso <kevin.vm1511@gmail.com>
2024-05-03 02:27:28 -0600

    hanoip: Update info vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-# Clonar el repositorio packages_apps_MotoActions desde la rama fourteen-qpr2 con profundidad 1
+# Clone harware
 git clone --depth=1 https://github.com/Sozosha-Dev/packages_apps_MotoActions -b fourteen-qpr2 packages/apps/MotoActions
 
-# Clonar el repositorio android_system_qcom en el directorio system/qcom
+# Clone system/qcom
 git clone https://github.com/LineageOS/android_system_qcom system/qcom
 
-# Clonar el repositorio packages_apps_ViPER4AndroidFX
+# Clone ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
-# Clonar bcr
+# Clone bcr
 git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
 
-# Configurar el uso de ccache
+# Clone vendor OSS
+git clone https://github.com/Hanoip-Development/proprietary_vendor_motorola_hanoip vendor/motorola/hanoip
+
+# Clone Kernel
+git clone --depth=1 https://github.com/Sozosha-Dev/kernel_motorola_sm6150 kernel/motorola/hanoip
+
+# CCache
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 50G
 ccache -o compression=true
-
-# Desayunar el dispositivo hanoip
-breakfast hanoip
